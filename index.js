d41f02206b8bf852a23d0327895a899fe5c0ef54
Jennifer Kramer <jenkrame@gmail.com>
2020-09-11 13:14:39 -0400

    completed question 3 in javascript sprint2

--- index.js
+++ index.js
@@ -227,19 +227,18 @@ console.log(artists[8]);
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
+    
+    return array[index]
   }
-  
+console.log(getArtistByIndex(artists, 0));
   /**
-
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
-
-  /* Code here */
+function get20s(data){
 
 }
+console.log(get20s(artists))
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
